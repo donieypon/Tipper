6dca339185eaac7f467b8346102cb1c0d0afc07f
Donie Ypon <donieypon@rauls-mbp-2.attlocal.net>
2018-08-19 21:11:13 -0700

    added .gitignore .gitattributes

--- Tipper/ViewController.swift
+++ Tipper/ViewController.swift
@@ -8,11 +8,36 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    @IBOutlet weak var billView: UIView!
+    @IBOutlet weak var tipView: UIView!
+    @IBOutlet weak var totalView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        billField.becomeFirstResponder()
+        
+        self.billView.layer.borderWidth = 1
+        self.billView.layer.borderColor = UIColor(red:100/255, green:225/255, blue:100/255, alpha: 1).cgColor
+        
+        self.tipView.layer.borderWidth = 1
+        self.tipView.layer.borderColor = UIColor(red:100/255, green:225/255, blue:100/255, alpha: 1).cgColor
+        
+        self.totalView.layer.borderWidth = 1
+        self.totalView.layer.borderColor = UIColor(red:100/255, green:225/255, blue:100/255, alpha: 1).cgColor
+        
+        //trying to make cool segment controller
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +45,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        
+        let tipPercentages = [0.18, 0.2, 0.25]
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
 }
 
